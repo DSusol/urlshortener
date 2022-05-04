08cdecb668a92143b3737d4163967e87a3ede42d
susolda <dmity.a.susol@gmail.com>
2022-05-04 21:54:20 +0300

    testcontainers setup

---
+++ src/test/java/com/learning/urlshortener/TestContainerSupplier.java
@@ -0,0 +1,25 @@
+package com.learning.urlshortener;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public class TestContainerSupplier {
+
+    @Container
+    public static final PostgreSQLContainer container = new PostgreSQLContainer<>("postgres:13")
+            .withReuse(true)
+            .withExposedPorts(5432);
+
+    @DynamicPropertySource
+    public static void overrideProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
+
+
+    }
+}
--- src/test/java/com/learning/urlshortener/database/BaseDBTest.java
+++ src/test/java/com/learning/urlshortener/database/BaseDBTest.java
@@ -1,9 +1,13 @@
 package com.learning.urlshortener.database;
 
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.learning.urlshortener.TestContainerSupplier;
+
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ComponentScan("com.learning.urlshortener.database")
-public class BaseDBTest {
+public class BaseDBTest extends TestContainerSupplier {
 }
