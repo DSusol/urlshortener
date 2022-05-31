0b78181368a1beeaa0579658d2e520208bf43b95
DSusol <dmitry.a.susol@gmail.com>
2022-05-31 14:52:59 +0300

    Command java doc

--- src/main/java/com/learning/urlshortener/bot/commands/Command.java
+++ src/main/java/com/learning/urlshortener/bot/commands/Command.java
@@ -1,5 +1,17 @@
 package com.learning.urlshortener.bot.commands;
 
+/**
+ * List of telegram bot executable commands.
+ * <p></p>
+ * <b>Global</b> commands are the informing commands:
+ * <p><i>/start</i> - shows welcome message</p>
+ * <p><i>/help</i> - provides list of main commands</p>
+ * <p></p>
+ * <b>Main</b> commands are used to perform link operations:
+ * <p><i>/*_link</i> - link operation command</p>
+ * <p></p>
+ * DEFAULT is used as a marker for MultiStepCommandHandler.
+ */
 public enum Command {
     DEFAULT,
     START,
