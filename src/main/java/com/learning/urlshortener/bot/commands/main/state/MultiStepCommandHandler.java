41f8a356a84eb89d55f9b6755e4bad53bfd7ded9
DSusol <dmitry.a.susol@gmail.com>
2022-06-01 13:50:17 +0300

    insignificant code refactoring

--- src/main/java/com/learning/urlshortener/bot/commands/main/state/MultiStepCommandHandler.java
+++ src/main/java/com/learning/urlshortener/bot/commands/main/state/MultiStepCommandHandler.java
@@ -5,6 +5,7 @@ import static com.learning.urlshortener.bot.commands.CommandType.DEFAULT;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.BiFunction;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
@@ -26,16 +27,22 @@ public class MultiStepCommandHandler {
 
     public void setChatExecutingCommand(Long chatId, CommandType commandType) {
         ChatMetaData metaData = chatStates.get(chatId);
-        if (metaData != null) {
-            metaData.setCommandType(commandType);
-            chatStates.put(chatId, metaData);
-            return;
+        if (metaData == null) {
+            metaData = applicationContext.getBean(ChatMetaData.class, chatId);
         }
 
-        metaData = applicationContext.getBean(ChatMetaData.class, chatId);
         metaData.setCommandType(commandType);
         chatStates.put(chatId, metaData);
+
+//        chatStates.compute(chatId, (id, data) -> {
+//            if(data == null) {
+//                data = applicationContext.getBean(ChatMetaData.class, id);
+//            }
+//            data.setCommandType(commandType);
+//            return data;
+//        });
     }
+
 
     public boolean updateApplicableForMultiStepProcessing(Update update) {
         ChatMetaData metaData = getChatMetaData(update);
--- src/main/java/com/learning/urlshortener/bot/utils/MessageHandlerImpl.java
+++ src/main/java/com/learning/urlshortener/bot/utils/MessageHandlerImpl.java
@@ -12,7 +12,7 @@ import lombok.AllArgsConstructor;
 
 @Component
 @AllArgsConstructor
-public class MessageHandlerImpl implements MessageHandler {
+class MessageHandlerImpl implements MessageHandler {
 
     private final MessageSource messageSource;
 
