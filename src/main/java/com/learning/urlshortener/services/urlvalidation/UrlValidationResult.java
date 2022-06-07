abc5e87dd0893ec822e15503302204edf84bba59
DSusol <dmitry.a.susol@gmail.com>
2022-06-08 00:06:28 +0300

    removing unnecessary annotations from UrlValidationResult

--- src/main/java/com/learning/urlshortener/services/urlvalidation/UrlValidationResult.java
+++ src/main/java/com/learning/urlshortener/services/urlvalidation/UrlValidationResult.java
@@ -1,8 +1,5 @@
 package com.learning.urlshortener.services.urlvalidation;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
 /**
  * List of url input statuses.
  * <p></p>
@@ -10,8 +7,6 @@ import lombok.RequiredArgsConstructor;
  * <p><i>/INVALID</i> - incorrect url syntax</p>
  * <p><i>/SHORT_NAME</i> - urlshortener app cannot make url shorter</p>
  */
-@Getter
-@RequiredArgsConstructor
 public enum UrlValidationResult {
 
     VALID,
