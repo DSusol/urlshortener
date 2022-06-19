7087bc855646e6362c27efbf03102d3cc27606b6
DSusol <dmitry.a.susol@gmail.com>
2022-06-19 14:05:42 +0300

    config file update - @Bean registration -> @Autowired

--- src/main/java/com/learning/urlshortener/bot/config/BotConfig.java
+++ src/main/java/com/learning/urlshortener/bot/config/BotConfig.java
@@ -1,5 +1,6 @@
 package com.learning.urlshortener.bot.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,8 +25,8 @@ public class BotConfig {
     private final WebHookSetter webHookSetter;
     private final UrlShortenerBot bot;
 
+    @Autowired
     @SneakyThrows
-    @Bean
     void telegramBotRegistration() {
         if (receiveUpdateOption.equals("polling")) {
             new TelegramBotsApi(DefaultBotSession.class).registerBot(bot);
